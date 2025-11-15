be3e0f43c51f7afeab24a8d604946bbae2d6326e
Surakuri Morimoto <Adriancarlcaerlang@gmail.com>
2025-11-15 23:22:56 +0800

    Adding some classes

---
+++ src/main/java/com/Surakuri/Domain/HomeCategorySection.java
@@ -0,0 +1,17 @@
+package com.Surakuri.Domain;
+
+public enum HomeCategorySection {
+
+
+    HOME,                            // Home Category
+    DEALS_PROMOTION,                 // Deals & Promotions
+    TOOLS_EQUIPMENT,                 // Tools & Equipment
+    BUILDING_CONSTRUCTION,           // Building & Construction
+    ELECTRICAL_LIGHTNING,            // Electrical & Lightning
+    PLUMBING_PIPES,                  // Plumbing & Pipes
+    GARDEN_OUTDOOR,                  // Garden & Outdoor
+    SPECIAL_CATEGORIES ,              // Special Categories
+
+    GRID
+
+}
