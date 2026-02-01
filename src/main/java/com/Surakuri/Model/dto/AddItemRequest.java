68b59975b22584a32c1e6c4ba514991be0e5c9cd
Surakuri Morimoto <Adriancarlcaerlang@gmail.com>
2026-02-01 16:44:28 +0800

    Chore: Refresh project structure and apply .gitignore

--- src/main/java/com/Surakuri/Model/dto/AddItemRequest.java
+++ src/main/java/com/Surakuri/Model/dto/AddItemRequest.java
@@ -3,7 +3,7 @@ package com.Surakuri.Model.dto;
 import lombok.Data;
 
 @Data
-public class AddItemRequest {
+public class  AddItemRequest {
     private Long variantId; // The specific SKU (e.g., "Red Wire ID")
     private int quantity;   // How many?
 }
